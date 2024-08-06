51fdacf31141f7f85dbd00beb3043cc981bf0be5
Fernii99 <asierfc@gmail.com>
2024-08-06 18:25:53 +0200

    [ADD] Button to see the details of the Superhero, Service Petition and display of the data

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero.interface';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { environment } from '../../../environments/environments';
 
 @Injectable({providedIn: 'root'})
@@ -17,6 +17,19 @@ export class HeroesService {
   //getHeroes Function that returns the Array of type Hero from the petition
   getHeroes():Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseURL}/heroes`)
+  }
+
+
+  //Function that will return an Observable of type Hero or undefined
+  //as the get method can return an 404 error if the id is not an existing one
+  // adding a pipe with a catchError(imported from rxjs) will detect and return what we need
+  //! Note that as the return is an observable we need to add the of() from rxjs to make it
+  //! become an observable, in other case it will say it is not type observable
+  getHeroById(id: string):Observable< Hero | undefined> {
+    return this.http.get<Hero>(`${this.baseURL}/heroes/${id}`)
+    .pipe(
+      catchError( error => of(undefined) )
+    )
   }
 
 }
